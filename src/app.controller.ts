cca6c2cb51821275f15db124368a5f0a59c17cc8
Peppa Pig <rohit.pandey@outlook.com>
2025-10-02 14:14:07 +1000

    finished chapter 07

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get("/")
+  @Render('index')
+  index() {
+      return {
+          title: "Home Page - Online Store"
+      };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/about")
+    @Render('about')
+    about() {
+      let viewData = [];
+      viewData["description"] = "This is an about page ...";
+      viewData["author"] = "Developed by: Varon Tiwari";
+      let data1 = 'About us - Online Store';
+      return {
+          title: data1,
+          subTitle: "About us",
+          viewData: viewData
+      };
+
   }
 }
