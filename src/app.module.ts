5d523412669bc43aa0423e0524593f35d2321980
Peppa Pig <rohit.pandey@outlook.com>
2025-10-02 14:35:40 +1000

    added product controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ProductsController } from './products.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController],
 })
 export class AppModule {}
