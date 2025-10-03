6ff97b878430e17dcb6b9467b6e7b52f775f2ad4
Peppa Pig <rohit.pandey@outlook.com>
2025-10-03 15:16:56 +1000

    added listing Products with Database Data

---
+++ src/models/products.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Product} from "./product.entity";
+
+@Injectable()
+export class ProductsService {
+    constructor(@InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>) {}
+
+    findAll(): Promise<Product[]> {
+        return this.productsRepository.find();
+    }
+
+    findOne(id: string): Promise<Product> {
+        return this.productsRepository.findOne(id);
+    }
+}
